c58cdd59dd3ae04bff216becba0ecb3c7bdad3eb
Eloy Gomez <eloy@indeos.es>
2014-09-07 21:19:40 +0200

    Add on_exit callback

--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,12 @@
 #include "domoio.h"
 
 
+void on_exit() {
+  domoio::exit_domoio();
+}
+
 int main(int argc, char* argv[]) {
+
 
   domoio::setup_config_options();
   domoio::prepare_config(argc, argv);
@@ -9,5 +14,9 @@ int main(int argc, char* argv[]) {
   // JS service
   // domoio::run();
   domoio::init_domoio();
+
+  // Exit callback
+  std::atexit(on_exit);
+
   domoio::run_server();
 }
