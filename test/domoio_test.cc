c471fed1a146a3aed92d140d1e8c44bebb2e9948
Eloy Gomez <eloy@indeos.es>
2015-01-05 23:33:27 +0100

    Clean database after each test

--- test/domoio_test.cc
+++ test/domoio_test.cc
@@ -1,9 +1,34 @@
 #include "domoio_test.h"
 
+
+class DatabaseCleaner : public ::testing::EmptyTestEventListener {
+  // Called before a test starts.
+  virtual void OnTestStart(const ::testing::TestInfo& test_info) {
+    domoio::db::exec_update("truncate table users");
+    domoio::db::exec_update("truncate table devices");
+  }
+
+  // Called after a failed assertion or a SUCCEED() invocation.
+  virtual void OnTestPartResult(const ::testing::TestPartResult& test_part_result) {
+  }
+
+  // Called after a test ends.
+  virtual void OnTestEnd(const ::testing::TestInfo& test_info) {
+  }
+};
+
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   ::testing::Environment* const env = ::testing::AddGlobalTestEnvironment(new domoio::TestEnvironment);
-  domoio::setup_config_options();
+
+  domoio::setup_config_options("config_test");
   domoio::prepare_config(argc, argv);
+
+  // Gets hold of the event listener list.
+  ::testing::TestEventListeners& listeners = ::testing::UnitTest::GetInstance()->listeners();
+  // Adds a listener to the end.  Google Test takes the ownership.
+  listeners.Append(new DatabaseCleaner);
+
   return RUN_ALL_TESTS();
 }
